662c75a3c023734e1246ecb82f5e39dc9b385ae8
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2021-01-28 13:04:58 +0700

    feat: change mapping little bit

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -6,6 +6,9 @@ vim.g.maplocalleader = " "
 
 -- Main Configuration
 remap("n", "<Leader>r", "<CMD>luafile %<CR>", { noremap = true })
+remap("n", "<Leader>q", ":bd <CR>", { noremap = true })
+remap("n", "<M-q>", ":bp <CR>", { noremap = true })
+remap("n", "<M-w>", ":bn <CR>", { noremap = true })
 
 -- Telescope
 remap('n', '<C-p>', '<cmd>lua require("telescope.builtin").find_files()<cr>', { noremap = true, silent = true })
